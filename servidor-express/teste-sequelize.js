f745f7d6eac2720c5ef147152c8da4e398799656
Isaac <isaacbarros88@gmail.com>
2022-08-04 13:59:14 -0300

    Criação de Models

--- servidor-express/teste-sequelize.js
+++ servidor-express/teste-sequelize.js
@@ -9,3 +9,42 @@ sequelize.authenticate().then(function(){
 }).catch(function(erro){
     console.log("Falha ao se conectar: ", erro);
 })
+
+const Postagem = sequelize.define('postagens', {
+    titulo: {
+        type: Sequelize.STRING
+    },
+    conteudo: {
+        type: Sequelize.TEXT 
+    }
+})
+
+// Postagem.sync({force:true})
+// Postagem.create({
+//     titulo: "Titulo qualquer",
+//     conteudo: "Um conteudo qualquer"
+// })
+
+
+const Usuario = sequelize.define('usuarios', {
+    nome: {
+        type: Sequelize.STRING
+    },
+    sobrenome: {
+        type: Sequelize.STRING
+    },
+    idade: {
+        type: Sequelize.INTEGER
+    },
+    email: {
+        type: Sequelize.STRING
+    }
+})
+
+// Usuario.sync({force:true})
+Usuario.create({
+    nome: "Isaac",
+    sobrenome: "Barros",
+    idade: 24,
+    email: "isaac@mail.com"
+})
