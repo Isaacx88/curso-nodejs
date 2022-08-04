b82d689d524cbcc950320fe7d08d272cbd655e1f
Isaac <isaacbarros88@gmail.com>
2022-08-03 22:32:15 -0300

    Rotas e parametros com express

--- servidor-express/index.js
+++ servidor-express/index.js
@@ -9,9 +9,15 @@ app.get("/about", function(req, res){
     res.send("About me");
 });
 
-app.get("/blog", function(req, res){
+app.get('/blog', function(req, res){
     res.send("Welcome to my blog");
 });
+
+app.get('/ola/:nome/:cargo', function(req, res){
+    //o send pode ser usado somente uma vez
+    res.send("<h1> Ola " + req.params.nome + "</h1>" + "<h2> Seu cargo: " + req.params.cargo + "</h2>");    
+
+})
 
 
 app.listen(8081, function(){
